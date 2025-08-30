ac8f9f151f3411bb34a32ec5c4868b27593fa195
marforitheus-a11y <marforitheus@gmail.com>
2025-08-30 12:26:44 -0300

    feat(ui): match resultados view to quiz option markup + utility icon classes

--- resultados.js
+++ resultados.js
@@ -47,7 +47,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
             reviewHTML += `
                 <li class="option ${className}">
-                    <span class="option-letter">${letters[optionIndex]}</span>
+                    <span class="option-letter">
+                      <span class="letter">${letters[optionIndex]}</span>
+                      <svg class="icon icon-check" width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path d="M20 6L9 17l-5-5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                      <svg class="icon icon-cross" width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path d="M18 6L6 18M6 6l12 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                    </span>
                     <span class="option-text">${option}</span>
                 </li>
             `;
